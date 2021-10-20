918bcea9207c63d1e4d8d37338983bdf3b4af3d1
cristianbambague <criistiianb10@gmail.com>
2021-10-20 13:31:24 -0500

    avance

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { inquirerMenu,
+        pausa, 
+        leerInput} = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 require('colors');
 
@@ -9,10 +13,44 @@ const main = async() => {
     console.log('Hola mundo.');
     
     let opt = '';
+    const tareas = new Tareas();
+    
+    // tareas._listado[tarea.id] = tarea;
+
+    // console.log(tareas);
 
     do {
+        //Imprimir el menu.
         opt = await inquirerMenu();
-        console.log( opt );
+
+        switch ( opt ) {
+            case '1':
+                TODO: 'Crear tareas.'
+                    const desc = await leerInput('Descripción: ');
+                    tareas.crearTarea(desc);
+
+                break;
+            case '2':
+                TODO: 'listar las tareas creadas.'
+                console.log( tareas.listadoArr);
+                break;
+            case '3':
+                
+                break;
+            case '4':
+                
+                break;
+            case '5':
+                
+                break;
+            case '6':
+                
+                break;
+
+            default: 0
+                break;
+        }
+
         await pausa();
 
     } while ( opt !== '0' );
